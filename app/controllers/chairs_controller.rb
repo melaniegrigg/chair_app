dc58150add94c1343775ee4365eee88e85bfc1d5
melaniegrigg <melanie.grigger@gmail.com>
2020-07-22 20:19:52 -0400

    add new and create actions

--- app/controllers/chairs_controller.rb
+++ app/controllers/chairs_controller.rb
@@ -9,4 +9,20 @@ class ChairsController < ApplicationController
     @chair = Chair.find_by(id: params[:id])
     render 'show.html.erb'
   end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    @chair = Chair.new(
+      color: params[:color],
+      weight: params[:weight],
+      legs: params[:legs],
+      style: params[:style],
+      image: params[:image]
+    )
+    @chair.save
+    redirect_to "/chairs/#{@chair.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
     get "/chairs" => "chairs#index"
+    get "/chairs/new" => "chairs#new"
+    post "/chairs" => "chairs#create"
     get "/chairs/:id" => "chairs#show"
 end
